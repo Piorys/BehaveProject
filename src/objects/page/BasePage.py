4fd307303939d2124bd14d0cb47a8217bbcb1c97
Piotr <piotr.ryszewski@accenture.com>
2020-01-16 15:08:20 +0100

    Updated BasePage

--- src/objects/page/BasePage.py
+++ src/objects/page/BasePage.py
@@ -7,14 +7,27 @@ from src.config import TestConf
 class BasePage(object):
     """ Page objects are to inherit from BasePage """
 
-    url = ""
     driver = TestConf.driver
+
+    def __init__(self):
+        pass
+
+    def find_element(self, locator):
+        return self.driver.find_element(*locator)
 
     def wait_until_visible(self, locator, timeout=TestConf.timeout):
         WebDriverWait(self.driver, timeout).until(ec.visibility_of_element_located(locator))
 
     def wait_until_clickable(self, locator, timeout=TestConf.timeout):
         WebDriverWait(self.driver, timeout).until(ec.element_to_be_clickable(locator))
+
+    def wait_until_visible_and_send_keys(self, locator, keys):
+        self.wait_until_visible(locator)
+        self.find_element(locator).clear()
+        self.find_element(locator).send_keys(keys)
+
+    def navigate_to_by_url(self, url):
+        self.driver.get()
 
     def is_visible(self, locator, timeout=1):
         try:
