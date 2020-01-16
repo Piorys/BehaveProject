edd5983fbcf9ef248694cdaaf8384655c1308a58
Piotr <piotr.ryszewski@accenture.com>
2020-01-16 18:39:34 +0100

    Modified TestConf

--- src/config/TestConf.py
+++ src/config/TestConf.py
@@ -1,3 +1,14 @@
 from selenium import webdriver
+
+# Default timeout
 timeout = 30
+
+# Which web driver is to be used
 driver = webdriver.Chrome
+
+# Logger file info
+output_dir = '/output'
+logger_file_name = '/logger.log'
+
+# Environment
+env = "www.twitter.com"
