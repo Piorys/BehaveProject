27a3465ec06f1f5a631571c8476ff3495df82369
Piotr <piotr.ryszewski@accenture.com>
2020-01-17 13:57:47 +0100

    Added Steps to first scenario

---
+++ features/steps/PostSteps.py
@@ -0,0 +1,11 @@
+from behave import *
+
+
+@when(u'I create a new tweet')
+def step_impl(context):
+    pass
+
+
+@then(u'I can see new tweet should be visible on my personal feed')
+def step_impl(context):
+    pass
